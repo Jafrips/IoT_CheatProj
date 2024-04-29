185a04690b6f726744bd407affa8263045c0b604
Jafrips <64084481+Jafrips@users.noreply.github.com>
2024-04-29 12:06:43 +0800

    Update Main.cs
    +MQTTnet library
    Data transfer to server (about close unspotted enemies)
    And example of json transfer (data about enemy)

--- Main.cs
+++ Main.cs
@@ -1,7 +1,12 @@
 using Swed64;
 using System;
 using System.Numerics;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using Newtonsoft.Json;
 
 // init memory for lib class
 Swed swed = new("cs2");
@@ -21,16 +26,29 @@ int m_entitySpottedState = 0x2278 + 0x8; // EntitySpottedState_t (spottedstate +
                                          // m_bSpotted = 0x8; // bool
                                          // m_bSpottedByMask = 0xC; // uint32[2]
 
-// positioning vars
+// variables
 Vector3 Pos = new Vector3();
 Vector3 LocalPos = new Vector3();
-float Distance;
-// other vars
+float Distance = 0;
 String PlayerTeam = "";
 String LocalPlayerTeam = "";
 bool IsSpotted = false;
 bool IsLocalPlayer = false;
 bool Danger = false; // for vibro engine
+uint health = 0;
+uint team = 0;
+string name = "";
+
+// MQTTnet variables
+var factory = new MqttFactory();
+var clientMqtt = factory.CreateMqttClient();
+
+var options = new MqttClientOptionsBuilder()
+    .WithTcpServer("cheatreciever.cloud.shiftr.io")
+    .WithCredentials("cheatreciever", "Slsk3E6qcEI0mM84")
+    .Build();
+
+await clientMqtt.ConnectAsync(options);
 
 while (true)
 {
@@ -68,7 +86,7 @@ while (true)
         IsLocalPlayer = swed.ReadBool(currentController, m_bIsLocalPlayerController);
 
         // get player team
-        uint team = swed.ReadUInt(currentPawn, m_iTeamNum);
+        team = swed.ReadUInt(currentPawn, m_iTeamNum);
         if (team == 3)
             PlayerTeam = "CT";
         else if (team == 2)
@@ -85,7 +103,7 @@ while (true)
         else
         {
             // get pawn health
-            uint health = swed.ReadUInt(currentPawn, m_iHealth);
+            health = swed.ReadUInt(currentPawn, m_iHealth);
             if (!(health > 0))
                 continue; // skip if not alive
             else if (LocalPlayerTeam == PlayerTeam)
@@ -95,21 +113,41 @@ while (true)
             IsSpotted = swed.ReadBool(currentPawn, m_entitySpottedState);
 
             // get controller attributes
-            string name = swed.ReadString(currentController, m_iszPlayerName, 16); // 16 characters
+            name = swed.ReadString(currentController, m_iszPlayerName, 16); // 16 characters
 
             Pos = swed.ReadVec(currentPawn, m_pos);
             Distance = Vector3.Distance(LocalPos, Pos);
             //Console.WriteLine($"Distance to player: {Distance}");
 
-            Console.WriteLine($"{name}: {health}hp, team: {PlayerTeam}, Distance: {Distance}, Spotted: {IsSpotted}");
+            //Console.WriteLine($"{name}: {health}hp, team: {PlayerTeam}, Distance: {Distance}, Spotted: {IsSpotted}");
 
+            // vibro engine processing trigger if enemy is close and unspotted
             if (Distance < 164 && IsSpotted == false)
-            {
-                Console.WriteLine("Danger");
-            }
+                Danger = true;
+            else
+                Danger = false;
         }
     }
-    Console.WriteLine("-----------------------------------");
+
+    // MQTTnet PROCESSING (SENDING TO SERVER - shiftr.io)
+    var sendData = new List<string> { name, health.ToString(), Distance.ToString() };
+    // ДОРАБОТАТЬ - предусмотреть передачу массива по каждому врагу
+    // это как пример передачи массива через json
+    var jsonData = JsonConvert.SerializeObject(sendData);
+    var EnemyData = new MqttApplicationMessageBuilder()
+        .WithTopic("Server-data")
+        .WithPayload(jsonData)
+        .WithRetainFlag()
+        .Build();
+    await clientMqtt.PublishAsync(EnemyData);
+   
+    var DangerData = new MqttApplicationMessageBuilder()
+        .WithTopic("Danger-data")
+        .WithPayload(Danger.ToString())
+        .WithRetainFlag()
+        .Build();
+    await clientMqtt.PublishAsync(DangerData);
+
     // Задержка на 3 секунды
     Thread.Sleep(3000);
 }
